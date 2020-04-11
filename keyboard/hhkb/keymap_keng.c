861812da043561612be5b76b89793a28ed89a182
kengz <kengzwl@gmail.com>
2020-04-11 14:04:01 -0700

    update visual

--- keyboard/hhkb/keymap_keng.c
+++ keyboard/hhkb/keymap_keng.c
@@ -17,9 +17,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Alt   |Fn0|
      * `-----------------------------------------------------------'
-     *       |Alt|Gui  |         Space         |Gui  |Alt|
+     *       |Alt|Gui  |         Space         |Lef  |Rig|
      *       `-------------------------------------------'
      */
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
